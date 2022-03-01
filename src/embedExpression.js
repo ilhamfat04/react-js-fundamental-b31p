dceae138659f1e8cf3d85681c765ef3b488f1dc5
= <=>
2022-03-01 10:21:15 +0700

    update

--- src/embedExpression.js
+++ src/embedExpression.js
@@ -2,12 +2,22 @@ import React from "react";
 
 function EmbedExpression() {
   //Create function here
+  function getMajor() {
+    return "Fullstack Batch 31"
+  }
 
   //Create a variable here
+  const companyName = "Dumbways.id"
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>
+        Welcome to {companyName}
+        <br />
+        Class <h1>{getMajor()}</h1>
+      </p>
+    </div>
   );
 }
 
