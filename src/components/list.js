61fa9b75d4e4ef4537ce2a37ce942e337ae417b9
= <=>
2022-03-01 10:53:32 +0700

    update

--- src/components/list.js
+++ src/components/list.js
@@ -1,1 +1,14 @@
 // code Here
+// function List({ listData, action }) {
+function List(props) {
+    // console.log(props);
+    const { listData, action } = props
+    return (
+        <div>
+            <h1>{listData}</h1>
+            <button>{action}</button>
+        </div>
+    )
+}
+
+export default List
--- src/props.js
+++ src/props.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 //Import Components
+import List from "./components/list";
 
 function Props() {
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <List listData="Mercedez Benz" action="Sell" />
+      <List listData="BMW" action="Buy" />
+    </div>
   );
 }
 
